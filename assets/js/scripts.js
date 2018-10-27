de01d330c4261644b373d29e38c3c62bf912a6e6
WojtasF <wojtas.florczak@gmail.com>
2018-10-27 18:41:05 +0200

    v0.7
    added some animations,
    replace floating button with "go up" button,

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,24 +1,34 @@
-var options =
-[
-  {
-    icon:'1',
-    label: 'Run alert function',
-    callback: function() {
-      alert('dasd');
-    }
-  },
-  {
-    icon:'<i class="fa fa-id-card" aria-hidden="true"></i>',
-    label: 'Test Element 2'
-  },
-  {
-    icon:'3',
-    label: 'Test Element 3',
-    display: {
-      color: 'blue',
-      background: 'red'
-    }
-  }
-];
 
-var context = new Bubbler(options);
+$("[href='/#about']").click(function(){
+  $('html, body').animate({
+    scrollTop: $("#about").offset().top
+  }, 1200);
+})
+
+$("[href='/#projects']").click(function (){
+    $('html, body').animate({
+        scrollTop: $("#projects").offset().top
+    }, 1200);
+});
+
+$("[href='/#contact']").click(function (){
+  $('html, body').animate({
+      scrollTop: $("#contact").offset().top
+  }, 1200);
+});
+
+$('#goUp').click(function (){
+  $('html, body').animate({
+      scrollTop: 0
+  }, 1200);
+});
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+    if (document.body.scrollTop > 600 || document.documentElement.scrollTop > 600) {
+        $("#goUp").css("display", "block");
+    } else {
+        $("#goUp").css("display", "none");
+    }
+}
